6e0b33cd8d9b0c7a898876f3c3417c14a4ad25d6
Ruth Alvarado <69056343+ralvarado97@users.noreply.github.com>
2020-09-30 19:04:43 -0500

    Estructuras repetitivas.

--- code/javascript/06_estructuras_repetitivas.js
+++ code/javascript/06_estructuras_repetitivas.js
@@ -1,27 +1,31 @@
 let persona = {
-    nombre: "Alejandro David",
-    apellido: "Lopez Vega",
-    peso: 150.
+    nombre: 'Alejandro',
+    apellido: 'Vega',
+    peso: 150, 
 }
 
 const INCREMENTAR_PESO = 3
-const DECREMENTAR_PESO = 2 
+const DECREMENTAR_PESO = 2
 
 const aumentarPeso = ( objeto ) => objeto.peso += INCREMENTAR_PESO
-const disminuyePeso = ( objeto ) => objeto.peso += DECREMENTAR_PESO
-const comeMucho = () => Math.random() < 0.1
-const realizaDeporte = () => Math.random() < 0.8
+const disminuyePeso = ( objeto ) => objeto.peso -= DECREMENTAR_PESO
+
+const comeMucho = () => Math.random() < 0.5
+const realizaDeporte = () => Math.random() < 0.7
 
 const META = persona.peso - 10
-console.log( `Al inicio del año ${persona.nombre} pesa ${persona.peso}.`)
 
-while (persona.peso > META){
-    if (comeMucho()){
-        aumentarPeso(persona)
+console.log( `Al inicio del año ${persona.nombre} pesa ${persona.peso}.` )
+
+while( persona.peso > META ) {
+    if (comeMucho()) {
+        console.log('Aumenta el peso.')
+        aumentarPeso( persona )
     }
-    if (realizaDeporte()){
-        disminuyePeso(persona)
+    if (realizaDeporte()) {
+        console.log('Disminuye el peso.')
+        disminuyePeso( persona )
     }
 }
 
-console.log( `Al final del año ${persona.nombre} pesa ${persona.peso}.`)
+console.log( `Al final del año ${persona.nombre} pesa ${persona.peso}.` )
