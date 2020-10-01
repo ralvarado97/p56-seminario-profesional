90527cdc51b54ed35c4bc26e0f4182e56fb7b9ea
Ruth Alvarado <69056343+ralvarado97@users.noreply.github.com>
2020-10-01 13:45:54 -0500

    Verificacion de solicitudes y respuestas.

--- code/node-fundamentals/01_server/server.js
+++ code/node-fundamentals/01_server/server.js
@@ -8,18 +8,18 @@ app.use( bodyParser.urlencoded( {extended:false}))
 app.use( router )
 
 router.get('/carrera', function(req, res){
-    console.log( req.body )
-    console.log( req.query)
 
+    console.log( req.headers )
     res.send('Lista de Carreras de la UPS')
 })
 
-router.post('/carrera' , function(req, res){
-    console.log( req.body )
-    console.log( req.query)
-
-    res.status(201).send( { tipo_error:0, mensaje_error:'', mensaje_exito: 'Añadido Exitosamente.'})
-
+router.post('/carrera' , function( req, res ){
+    if (req.query.error == 'ok'){
+        res.status(500).send( { tipo_error:1, mensaje_error:'Error en el Servidor.', mensaje_exito: '' } )
+    } else {
+        res.status(200).send( { tipo_error:0, mensaje_error:'', mensaje_exito: 'Todo ok.'})
+    }
+   
 })
 app.use( '/' , express.static('public'))
 app.listen( 5000 )
